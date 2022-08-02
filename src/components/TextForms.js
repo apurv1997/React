9919fd1334ac6c3eb1bb50eff3a2bea6334dece0
Apurv Vyas <76184941+apurv1997@users.noreply.github.com>
2022-08-02 18:47:48 +0530

    update TextForms.js

--- src/components/TextForms.js
+++ src/components/TextForms.js
@@ -1,1 +1,61 @@
+import React, { useState } from "react";
+
+export default function TextForms(props) {
+  const handleUpClick = () => {
+    console.log("Uppercase was clicked" + text);
+    let newText = text.toUpperCase();
+    setText(newText);
+  };
+
+  const handleLoClick = () => {
+    let newText = text.toLowerCase();
+    setText(newText);
+  }
+
+  const handleClearText = () => {
+    let newText = '';
+    setText(newText);
+  }
+
+  const handleOnChange = (event) => {
+    console.log("On Change");
+    setText(event.target.value);
+  };
+  const [text, setText] = useState("Enter your text");
+
+  return (
+    <>
+      <div className="container">
+        <h1>{props.heading}</h1>
+        <div className="mb-3">
+          <textarea
+            className="form-control"
+            value={text}
+            onChange={handleOnChange}
+            id="myBox"
+            rows="8"
+          ></textarea>
+        </div>
+        <button className="btn btn-primary mx-1" onClick={handleUpClick}>
+          Convert to UpperCase
+        </button>
+        <button className="btn btn-primary mx-1" onClick={handleLoClick}>
+          Convert to LowerCase
+        </button>
+        <button className="btn btn-primary mx-1" onClick={handleClearText}>
+          Clear Text
+        </button>
+      </div>
+      <div className="container my-3">
+        <h1>Your Text Summary</h1>
+        <p>
+          {text.split(" ").length} words and {text.length} characters
+        </p>
+        <p>{0.008 * text.split(" ").length} Minutes read</p>
+        <h2>Preview</h2>
+        <p>{text}</p>
+      </div>
+    </>
+  );
+}
 
