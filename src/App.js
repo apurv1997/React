aa6c9fd228ea03d1cbb5a87c5e86a0736298456c
Apurv Vyas <76184941+apurv1997@users.noreply.github.com>
2022-08-02 18:45:07 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,1 +1,21 @@
+import logo from './logo.svg';
+import './App.css';
+import Navbar from './components/Navbar';
+import TextForms from './components/TextForms'
+import About from './components/About'
+
+function App() {
+  return (
+<>
+<Navbar title = "TextUtils" aboutText = "AboutText"/>
+{/* <Navbar /> */}
+{/* <div className="container my-3">
+<TextForms heading = "Enter the text to analyze"/>
+</div> */}
+<About/>
+</>
+  );
+}
+
+export default App;
 
